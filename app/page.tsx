a697fe657f4a37cfd0833be12c625dbee502e861
dev-xero <elo48858@gmail.com>
2024-04-24 20:02:52 +0100

    chore: Clean up imports

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Header from '@/components/Header';
 import InputField from '@/components/InputField';
 import TextBox from '@/components/TextBox';
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
     const [product, setProduct] = useState('');
