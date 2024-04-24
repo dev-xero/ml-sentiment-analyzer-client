c0743dee8c1a6973eba578a9ea65b716d386e5a5
dev-xero <elo48858@gmail.com>
2024-04-24 19:48:16 +0100

    fix: Increase gutter margins to 16px

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     const [product, setProduct] = useState('');
 
     return (
-        <main className="w-[calc(100%-16px)] h-full flex flex-col justify-center align-center mx-2">
+        <main className="w-[calc(100%-32px)] h-full flex flex-col justify-center align-center mx-4">
             <Header />
             <form action="/" method="POST" className='mt-12 w-[100%] max-w-lg mx-auto'>
                 <InputField
