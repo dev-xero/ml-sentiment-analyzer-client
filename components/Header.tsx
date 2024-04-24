8b77883e62e290b4cba7f8cd7dd8a8026eb6c9fe
dev-xero <elo48858@gmail.com>
2024-04-24 18:33:48 +0100

    fix: Incorrect header component resizing

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 export default function Header() {
     return (
-        <section className="flex items-center justify-center md:flex-row flex-col w-max-xl mx-auto">
+        <section className="flex items-center justify-center max-w-lg mx-2 sm:mx-auto">
             <Image
                 src={'/icon-smiley.svg'}
                 alt={'Icon Smiley'}
-                width={64}
-                height={64}
-                className="mb-3 md:mb-0 md:mr-4"
+                width={32}
+                height={32}
+                className="mr-3 md:mr-4 md:w-16 md:h-16"
             />
-            <h1 className="text-2xl md:text-3xl font-bold inline text-center">
+            <h1 className="text-xl md:text-2xl lg:text-3xl font-bold inline text-center">
                 AI Sentiment Analyzer
             </h1>
         </section>
