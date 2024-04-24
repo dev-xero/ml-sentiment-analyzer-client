36c328475a0011758ec92de3d19efe0642e18a8b
dev-xero <elo48858@gmail.com>
2024-04-24 18:24:01 +0100

    fix: Header component alignment

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,17 @@ import Image from 'next/image';
 
 export default function Header() {
     return (
-        <section className="flex items-center justify-center">
+        <section className="flex items-center justify-center md:flex-row flex-col w-max-xl mx-auto">
             <Image
                 src={'/icon-smiley.svg'}
                 alt={'Icon Smiley'}
-                width={32}
-                height={32}
-                className="mr-3 md:mr-4 md:w-16 md:h-16"
+                width={64}
+                height={64}
+                className="mb-3 md:mb-0 md:mr-4"
             />
-            <h1 className="text-2xl md:text-3xl font-bold inline">AI Sentiment Analyzer</h1>
+            <h1 className="text-2xl md:text-3xl font-bold inline text-center">
+                AI Sentiment Analyzer
+            </h1>
         </section>
     );
 }
